6ce3ec1d186b02f7f03cb37dfdb5297fa26d0418
kobaltcore <39258182+kobaltcore@users.noreply.github.com>
2019-05-28 19:50:40 +0200

    Actually make this stuff work
    A bunch of small fixes to make this function properly.
    The script now works on my Mojave 10.14.5 Hackintosh with CarbonCopyCloner.

--- EFIClone.sh
+++ EFIClone.sh
@@ -9,17 +9,17 @@
 # 2/24/2018
 # - Updated to use rsync to handle the deletion of information from the desination drive. Basically,
 #   allow rsync to do its thing
-# - Added additional check, based on clover boot log retrieved with bdmesg, to ensure that the 
-#   destination EFI partition is NOT the EFI partition that was used to boot the computer. 
+# - Added additional check, based on clover boot log retrieved with bdmesg, to ensure that the
+#   destination EFI partition is NOT the EFI partition that was used to boot the computer.
 # version 0.1beta2
 # 2/23/2018
 # Clean up and combine.
 # -Combined the two versions of the script into a single script that detects whether it was called from
-#      Carbon Copy Cloner or SuperDuper! and behaves accordingly in the setup. 
-# -Added validation of the copied data through SHASUM hashing of all visible files in the source and 
+#      Carbon Copy Cloner or SuperDuper! and behaves accordingly in the setup.
+# -Added validation of the copied data through SHASUM hashing of all visible files in the source and
 #      destination EFI partitions after the delete/rsync copy is complete
 # -Changed the method of copying files from cp to rsync
-# -Modified the copy path of the source to include the root directory - not just the /EFI/ directory 
+# -Modified the copy path of the source to include the root directory - not just the /EFI/ directory
 #      and subdirectories.
 #----------------------------------------------------------------------------------------------------------
 # version 0.1beta1
@@ -27,8 +27,8 @@
 # Initial Release
 #----------------------------------------------------------------------------------------------------------
 
-# This script is designed to be a "post-flight" script run automatically by CCC at the end of a 
-# clone task. It will copy the contents of the source drive's EFI partition to the destination drive's EFI 
+# This script is designed to be a "post-flight" script run automatically by CCC at the end of a
+# clone task. It will copy the contents of the source drive's EFI partition to the destination drive's EFI
 # partition. It will COMPLETELY DELETE and replace all data on the destination EFI partition.
 
 #!!! This script DOES delete data and therefore has the potential to cause unintended data loss.
@@ -41,13 +41,13 @@
 
 #Log File location. By default, this will write to the same directory where the cccEFIClone.sh script lives.
 # you may edit it, but be sure the location will always exist and you have permissions to write to the location
-# It appears that Carbon Copy Cloner runs the script in the root of the boot drive, so that is where the log will go 
+# It appears that Carbon Copy Cloner runs the script in the root of the boot drive, so that is where the log will go
 # if you don't put in a literal path below.
 LOG_FILE="$PWD/EFIClone.log"
 
 #test switch. The script is distributed with this switch set to "Y".
 # the script will not delete or copy any data when this is set to Y
-# in order to make the script take action, once you have verified that it can identify the right locations to 
+# in order to make the script take action, once you have verified that it can identify the right locations to
 # copy from and to on your system, change this variable to N
 TEST_SWITCH="Y"
 
@@ -56,7 +56,7 @@ TEST_SWITCH="Y"
 #----------------------------------------------
 
 function writeTolog () {
-	echo "[`date`] - ${*}" >> ${LOG_FILE} 
+	echo "[`date`] - ${*}" >> ${LOG_FILE}
 }
 
 function getDiskNumber () {
@@ -72,7 +72,7 @@ function getAPFSPhysicalDiskNumber () {
 }
 
 function getEFIVolume () {
-	echo "$( diskutil list | grep "$1" | grep "EFI" | rev | cut -d ' ' -f 1 | rev )"	
+	echo "$( diskutil list | grep "$1" | grep "EFI" | rev | cut -d ' ' -f 1 | rev )"
 }
 
 function getDiskMountPoint () {
@@ -108,15 +108,14 @@ function getDiskIDfromUUID () {
 
 #begin logging
 writeTolog "***** EFI Clone Script start"
-writeTolog "working directory = $PWD" 
-writeTolog "Running $0" 
+writeTolog "working directory = $PWD"
+writeTolog "Running $0"
 
 #determine which disk clone application called the script (based on number of parameters)
 # - log details
 # - set up initial parameters
 # - if possible do calling app-specific sanity checks in order to exit without taking action if necessary
-if [[ "$#" == "4" ]]
-then
+if [[ "$#" == "4" ]]; then
 	#log inputs to script
 	writeTolog "Called From Carbon Copy Cloner"
 	writeTolog "1: Source Path = $1"   # Source path
@@ -142,60 +141,60 @@ then
 		osascript -e 'display notification "CCC Clone destination was a disk image. Clone script did not run." with title "EFI Clone Script"'
 		exit 0
 	fi
- 
+
 	#set source and destination from variables passed in
 	sourceVolume=$1
 	destinationVolume=$2
-else	
-	if [[ "$#" == "6" ]]
-	then 
+else
+	if [[ "$#" == "6" ]]; then
 		#log inputs to script
 		writeTolog "Called From SuperDuper!"
-		writeTolog "1: Source Disk Name = $1"   
-		writeTolog "2: Source Mount Path = $2"   
-		writeTolog "3: Destination Disk Name = $3"   
-		writeTolog "4: Destination Mount Path = $4"  
+		writeTolog "1: Source Disk Name = $1"
+		writeTolog "2: Source Mount Path = $2"
+		writeTolog "3: Destination Disk Name = $3"
+		writeTolog "4: Destination Mount Path = $4"
 		writeTolog "5: SuperDuper! Backup Script Used = $5"
 		writeTolog "6: Unused parameter 6 = $6"
-		
+
 		#set source and destination from variables passed in
 		sourceVolume=$2
-		destinationVolume=$4	
+		destinationVolume=$4
 	else
 		#an unknown number of parameters have been passed in. log that fact and then exit
 		writeTolog "$# parameters were passed in. This is an unsupported number of parameters. Exiting now"
 		echo "$# parameters were passed in. This is an unsupported number of parameters. Exiting now"
 		osascript -e 'display notification "Unsupported set of parameters passed in. EFI Clone script did not run!" with title "EFI Clone Script"'
 		exit 0
-	fi 
+	fi
 fi
 
-writeTolog sourceVolume = $sourceVolume 
+writeTolog sourceVolume = $sourceVolume
 
 sourceVolumeDisk="$( getDiskNumber "$sourceVolume" )"
 
-writeTolog sourceVolumeDisk = $sourceVolumeDisk 
+writeTolog sourceVolumeDisk = $sourceVolumeDisk
 
-writeTolog destinationVolume = $destinationVolume 
+writeTolog destinationVolume = $destinationVolume
 
 destinationVolumeDisk="$( getDiskNumber "$destinationVolume" )"
 
-writeTolog destinationVolumeDisk = $destinationVolumeDisk 
-sourceDisk=$sourceVolumeDisk 
+writeTolog destinationVolumeDisk = $destinationVolumeDisk
+sourceDisk=$sourceVolumeDisk
 sourceEFIPartition="$( getEFIVolume "$sourceDisk" )"
+
 #If we don't find an EFI partition on the disk that was identified by the volume path,
 # then check to see if it is a coreStoreage volume and get the disk number from there
 if [[ "$sourceEFIPartition" == "" ]]; then
     sourceDisk=""
 	sourceDisk=disk"$( getCoreStoragePhysicalDiskNumber "$sourceVolumeDisk" )"
-	if [[ "$sourceDisk" == "disk" ]]
-	then
+	if [[ "$sourceDisk" == "disk" ]]; then
 		sourceDisk=""
-		sourceDisk=$sourceVolumeDisk 
-	fi 
+		sourceDisk=$sourceVolumeDisk
+	fi
 	sourceEFIPartition="$( getEFIVolume "$sourceDisk" )"
 fi
-#If we still don't have an EFI partition then look to see if the sourceVolumeDisk is an APFS 
+
+#If we still don't have an EFI partition then look to see if the sourceVolumeDisk is an APFS
 # volume and find the physical disk
 if [[ "$sourceEFIPartition" == "" ]]; then
     sourceDisk=""
@@ -203,89 +202,87 @@ if [[ "$sourceEFIPartition" == "" ]]; then
 	sourceEFIPartition="$( getEFIVolume "$sourceDisk" )"
 fi
 
-writeTolog sourceEFIPartition = $sourceEFIPartition 
+writeTolog sourceEFIPartition = $sourceEFIPartition
 
-destinationDisk=$destinationVolumeDisk 
+destinationDisk=$destinationVolumeDisk
 destinationEFIPartition="$( getEFIVolume "$destinationDisk" )"
+
 #If we don't find an EFI partition on the disk that was identified by the volume path,
 # then check to see if it is a coreStoreage volume and get the disk number from there
 if [[ "$destinationEFIPartition" == "" ]]; then
 	destinationDisk=""
 	destinationDisk=disk"$( getCoreStoragePhysicalDiskNumber "$destinationVolumeDisk" )"
-	if [[ "$destionationDisk" == "disk" ]]
-	then
+	if [[ "$destinationDisk" == "disk" ]];	then
 		destinationDisk=""
 		destinationDisk=$destinationVolumeDisk
 	fi
-	destinationEFIPartition="$( getEFIVolume "$sourceDisk" )"
+	destinationEFIPartition="$( getEFIVolume "$destinationDisk" )"
 fi
-#If we still don't have an EFI partition then look to see if the sourceVolumeDisk is an APFS 
+
+#If we still don't have an EFI partition then look to see if the sourceVolumeDisk is an APFS
 # volume and find the physical disk
 if [[ "$destinationEFIPartition" == "" ]]; then
 	destinationDisk=""
 	destinationDisk=disk"$( getAPFSPhysicalDiskNumber "$destinationVolumeDisk" )"
-	destinationEFIPartition="$( getEFIVolume "$sourceDisk" )"
+	destinationEFIPartition="$( getEFIVolume "$destinationDisk" )"
 fi
 
-writeTolog destinationEFIPartition = $destinationEFIPartition 
+writeTolog destinationEFIPartition = $destinationEFIPartition
 
 efiBootPartitionUUID="$( getCurrentBootEFIVolumeUUID )"
 writeTolog "efiBootPartitionUUID = $efiBootPartitionUUID"
 efiBootPartionDisk="$( getDeviceIDfromUUID "$efiBootPartitionUUID" )"
 writeTolog "efiBootPartitionDisk = $efiBootPartionDisk"
-if [[ "$efiBootPartitionDisk" == "$destinationDisk" ]]
-then
+
+if [[ "$efiBootPartitionDisk" == "$destinationDisk" ]]; then
 	writeTolog "Destination disk is the current EFI partition that was used to boot the computer, script exiting."
 	osascript -e 'display notification "No source EFI Partition found. EFI Clone Script did not run!." with title "EFI Clone Script"'
-fi 
+fi
 
-if [[ "$sourceEFIPartition" == "" ]]
-then
+if [[ "$sourceEFIPartition" == "" ]]; then
 	writeTolog "No SourceEFIPartition Found, script exiting."
 	osascript -e 'display notification "No source EFI Partition found. EFI Clone Script did not run!." with title "EFI Clone Script"'
 	exit 0
-fi 
+fi
 
-if [[ "$destinationEFIPartition" == "" ]]
-then
+if [[ "$destinationEFIPartition" == "" ]]; then
 	writeTolog "No DestinationEFIPartition Found, script exiting."
 	osascript -e 'display notification "No destination EFI Partition found. EFI Clone Script did not run!." with title "EFI Clone Script"'
 	exit 0
 fi
 
-if [[ "$sourceEFIPartition" == "$destinationEFIPartition" ]]
-then
+if [[ "$sourceEFIPartition" == "$destinationEFIPartition" ]]; then
 	writeTolog "Source and Destination EFI Partitions are the same. Script exiting."
 	osascript -e 'display notification "Source and Destination EFI partitions are the same. EFI Clone Script did not run!." with title "EFI Clone Script"'
 	exit 0
 fi
 
-
 diskutil mount /dev/$sourceEFIPartition
 diskutil mount /dev/$destinationEFIPartition
 writeTolog "drives Mounted"
 sourceEFIMountPoint="$( getDiskMountPoint "$sourceEFIPartition" )"
-writeTolog sourceEFIMountPoint = $sourceEFIMountPoint 
+writeTolog sourceEFIMountPoint = $sourceEFIMountPoint
 
 destinationEFIMountPoint="$( getDiskMountPoint "$destinationEFIPartition" )"
-writeTolog destinationEFIMountPoint = $destinationEFIMountPoint 
-if [[ "$TEST_SWITCH" == "Y" ]]
-then
+writeTolog destinationEFIMountPoint = $destinationEFIMountPoint
+
+if [[ "$TEST_SWITCH" == "Y" ]]; then
 	writeTolog "********* Test simulation - file delete/copy would happen here. "
 	writeTolog "rsync command will be executed with the --dry-run option"
 	writeTolog "rsync command calculated is..."
 	writeTolog "rsync -av --exclude='.*'' "$sourceEFIMountPoint/" "$destinationEFIMountPoint/""
 	writeTolog "THE BELOW OUTPUT IS FROM AN RSYNC DRY RUN! NO DATA HAS BEEN MODIFIED!"
-	rsync --dry-run -av --exclude=".*" --delete "$sourceEFIMountPoint/" "$destinationEFIMountPoint/" >> ${LOG_FILE} 
+	rsync --dry-run -av --exclude=".*" --delete "$sourceEFIMountPoint/" "$destinationEFIMountPoint/" >> ${LOG_FILE}
 	writeTolog "********* Test Simulation - end of file delete/copy section."
-else 
+else
 	writeTolog "Synchronizing all files with rsync --delete option"
 	writeTolog "from $sourceEFIMountPoint/EFI to $destinationEFIMountPoint. Details follow..."
 	writeTolog "--------------------------------------------------------------------"
 	rsync -av --exclude=".*" --delete "$sourceEFIMountPoint/" "$destinationEFIMountPoint/" >> ${LOG_FILE}
 	writeTolog "--------------------------------------------------------------------"
 	writeTolog "Contents of Source EFI Partition copied to Destination EFI Partition"
-fi 
+fi
+
 writeTolog "Compare the checksums of the EFI directories on the source and destination partitions"
 writeTolog "-------------------------------------------------------------------------------------"
 pushd "$sourceEFIMountPoint/"
@@ -299,23 +296,25 @@ writeTolog "Destination directory hash: $destinationEFIHash"
 temp="$( logEFIDirectoryHashDetails "$sourceEFIMountPoint" )"
 popd
 
-if [[ "$sourceEFIHash" == "$destinationEFIHash" ]]
-then
-	writeTolog "Directory hashes match! file copy successful"
-else
-	writeTolog "Directory hashes differ! file copy unsuccessful"
-fi 
+if [[ "$TEST_SWITCH" != "Y" ]]; then
+	if [[ "$sourceEFIHash" == "$destinationEFIHash" ]]; then
+		writeTolog "Directory hashes match! file copy successful"
+	else
+		writeTolog "Directory hashes differ! file copy unsuccessful"
+	fi
+fi
+
 writeTolog "-------------------------------------------------------------------------------------"
 diskutil unmount /dev/$destinationEFIPartition
 diskutil unmount /dev/$sourceEFIPartition
 writeTolog "EFI Partitions Unmounted"
 writeTolog "EFIClone.sh complete"
-if [[ "$sourceEFIHash" == "$destinationEFIHash" ]]
-then
-	osascript -e 'display notification "EFI Clone Script completed successfully." with title "EFI Clone Script"'
-else
-	osascript -e 'display notification "EFI Clone failed - destionation data did not match source after copy." with title "EFI Clone Scipt"'
-fi 
 
-
-exit 0
+if [[ "$TEST_SWITCH" != "Y" ]]; then
+	if [[ "$sourceEFIHash" == "$destinationEFIHash" ]]; then
+		osascript -e 'display notification "EFI Clone Script completed successfully." with title "EFI Clone Script"'
+	else
+		osascript -e 'display notification "EFI Clone failed - destionation data did not match source after copy." with title "EFI Clone Scipt"'
+		exit 1
+	fi
+fi
